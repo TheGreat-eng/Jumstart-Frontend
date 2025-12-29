2412b33af4b0fcff33f78ff9d07b0fbdb668a61b
TheGreat <mirindano111@gmail.com>
2025-12-29 14:57:24 +0700

    feat:add first code

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p>Hello World!!!</p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
